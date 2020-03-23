7bebf384e8710ac467ca6fcf7661f54a334879c2
Zenichi Amano <crow.misia@gmail.com>
2020-03-23 21:09:33 +0900

    Remove identical code folding

--- core/src/main/jni/Application.mk
+++ core/src/main/jni/Application.mk
@@ -2,4 +2,4 @@ APP_ABI := all
 APP_OPTIM := release
 APP_CFLAGS += -O3 -ffunction-sections -fdata-sections -DLIBYUV_LEGACY_TYPES
 APP_CPPFLAGS += -O3 -ffunction-sections -fdata-sections -fvisibility=hidden -fvisibility-inlines-hidden
-APP_LDFLAGS += -Wl,--gc-sections,--icf=safe
+APP_LDFLAGS += -Wl,--gc-sections
