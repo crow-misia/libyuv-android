edc2a4cf9bc22e4dcd5c9b8ed28f74d04d899a7f
Zenichi Amano <crow.misia@gmail.com>
2020-05-12 23:04:45 +0900

    bump to gradle plugin 3.6.3

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val kotlin = "1.3.72"
-    const val gradlePlugin = "3.6.1"
+    const val gradlePlugin = "3.6.3"
     const val bintrayPlugin = "1.8.4"
 
     const val cmake = "3.10.2"
