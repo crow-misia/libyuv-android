59e007c0ba5a83cf7a7d4b06a92c3a96c2280968
Zenichi Amano <crow.misia@gmail.com>
2020-06-27 10:53:04 +0900

    update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,10 +1,10 @@
 object Versions {
     const val kotlin = "1.3.72"
-    const val gradlePlugin = "3.6.3"
+    const val gradlePlugin = "4.0.0"
     const val bintrayPlugin = "1.8.5"
 
-    const val ndk = "21.2.6472646"
-    const val buildTools = "29.0.3"
+    const val ndk = "21.3.6528147"
+    const val buildTools = "30.0.0"
     const val compileSdk = 29
     const val targetSdk = 29
     const val minSdk = 9
