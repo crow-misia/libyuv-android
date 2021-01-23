2ec0816c58d8a62784285e64554df6bc8c162987
Zenichi Amano <crow.misia@gmail.com>
2021-01-23 17:17:51 +0900

    dependencies update * gradle * kotlin * build-tools * ndk

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,12 +1,12 @@
 object Versions {
-    const val kotlin = "1.4.10"
-    const val gradlePlugin = "4.1.1"
+    const val kotlin = "1.4.21"
+    const val gradlePlugin = "4.1.2"
 
-    const val ndk = "21.3.6528147"
-    const val buildTools = "30.0.2"
+    const val ndk = "22.0.7026061"
+    const val buildTools = "30.0.3"
     const val compileSdk = 30
     const val targetSdk = 30
     const val minSdk = 9
 
-    const val name = "0.3.0"
+    const val name = "0.4.0"
 }
