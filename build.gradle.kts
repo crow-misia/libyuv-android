1d9c7bbc2ee7149b9abe4a5dc2df9ed10c77ccf8
Zenichi Amano <crow.misia@gmail.com>
2025-01-08 16:30:01 +0900

    build: ossrhからmaven central portalに移行する
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.dokka.javadoc) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.maven.publish) apply false
 }
