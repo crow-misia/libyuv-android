31a1626fe9e33817e355bd9af30f331f75c16a2c
Zenichi Amano <crow.misia@gmail.com>
2025-06-15 17:24:00 +0900

    chore: bump version v0.43.0 -> v0.43.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,7 +16,7 @@ object Maven {
     const val GROUP_ID = "io.github.crow-misia.libyuv"
     const val ARTIFACT_ID = "libyuv-android"
     const val DESCRIPTION = "LibYuv for Android"
-    const val VERSION = "0.43.0"
+    const val VERSION = "0.43.1"
     const val GITHUB_REPOSITORY = "crow-misia/libyuv-android"
 }
 
