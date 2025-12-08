dc55df5d28bc965481fd63fd25d2644b4173c19e
Zenichi Amano <crow.misia@gmail.com>
2025-12-08 15:26:01 +0900

    build: minSdkを23にアップデート
    androidxがminSdkを23にアップデートしたため
    
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- buildSrc/src/main/java/Build.kt
+++ buildSrc/src/main/java/Build.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Build {
     const val COMPILE_SDK = 36
-    const val MIN_SDK = 21
+    const val MIN_SDK = 23
     const val TARGET_SDK = 35
     const val NDK_VERSION = "28.1.13356709"
     val jvmTarget = JavaVersion.VERSION_11
